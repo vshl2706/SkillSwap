b3b3c38b02f109c8c40469aee734f25abff23256
Vishal Singh <126417458+vshl2706@users.noreply.github.com>
2025-05-28 00:11:43 +0530

    Created routes for getAllUsers, getUserById, deleteUser, updateUserRole

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
 import authorizeRoles from '../middleware/roleMiddleware.js';
-import { getProfile, updateProfile, getAllUsers} from '../controllers/userController.js';
+import { getProfile, updateProfile, getAllUsers, deleteUser, getUserById, updateUserRole} from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -9,6 +9,9 @@ const router = express.Router();
 router.route('/profile')
     .get(protect, getProfile)
     .put(protect, updateProfile);
-router.get('/',protect, authorizeRoles('admin'), getAllUsers);
+router.get('/',protect, authorizeRoles('admin'),getAllUsers);
+router.get('/:id',protect,authorizeRoles('admin'),getUserById);
+router.delete('/:id',protect, authorizeRoles('admin'),deleteUser);
+router.put('/:id',protect, authorizeRoles('admin'),updateUserRole);
 
 export default router;
